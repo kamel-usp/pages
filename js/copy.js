43d8524636843cdcdb389ba2820c9edcc82a4ee3
Renato Lui Geh <renatogeh@gmail.com>
2023-06-18 10:27:04 -0300

    Clipboard copy for firefox browsers

--- js/copy.js
+++ js/copy.js
@@ -3,19 +3,20 @@ const blocks = document.querySelectorAll("div.highlight");
 blocks.forEach(div => {
   const copyblk = document.createElement("div");
   copyblk.className = "copy";
+  copyblk.addEventListener("click", handleCopyClick);
   const copy = document.createElement("i");
   copy.className = "fa fa-clone"
   copy.setAttribute("aria-hidden", "true");
-  copy.addEventListener("click", handleCopyClick);
   copyblk.append(copy);
   div.append(copyblk);
 });
 
 function handleCopyClick(event) {
   var range = document.createRange();
-  range.selectNode(event.target.parentElement);
+  range.selectNode(event.target.parentElement.parentElement);
   window.getSelection().removeAllRanges();
   window.getSelection().addRange(range);
-  document.execCommand("copy");
+  if (!navigator.clipboard) document.execCommand("copy");
+  else navigator.clipboard.writeText(range);
   window.getSelection().removeAllRanges();
 }
